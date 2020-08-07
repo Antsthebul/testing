5fb841ea1e920f28f3af48af8989e6406fe413bb
whozaWhatza <enjoi4life411@yahoo.com>
2020-08-06 22:25:18 -0400

    lets see fetch now

--- fix.py
+++ fix.py
@@ -3,4 +3,6 @@
 I forgot last time but i want to run this
 to see the difference in get,. I think i can say
 fetch updates the logs and git pull merges the changes
+
+This is still very tough to understand
  '''
